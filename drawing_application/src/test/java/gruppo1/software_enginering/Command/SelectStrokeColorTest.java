ce1efbd8e5b893edc5965fecb843a11ddb3ffdef
Taddeux <l.taddeo5@studenti.unisa.it>
2022-12-08 19:05:17 +0100

    Commit delle seguenti funzioni di test:
    SelectFillColorTest
    SelectStrokeColorTest
    UpdateCommandTest
    MyEllipseTest
    MyLineTest
    MyRectangleTest
    ShapeFactoryTest

--- drawing_application/src/test/java/gruppo1/software_enginering/Command/SelectStrokeColorTest.java
+++ drawing_application/src/test/java/gruppo1/software_enginering/Command/SelectStrokeColorTest.java
@@ -15,20 +15,33 @@ public class SelectStrokeColorTest {
     void testExecute() {
         Color strokeColor = new Color(0, 0, 0, 0);
         // Color error = new Color(1, 0, 0, 0);
+        System.out.println("Color :" +strokeColor);
+
+        System.out.println("\n--------  TEST CASE 1  --------\n");
 
         Rectangle rectangle = new Rectangle();
+        System.out.println("Stroke color of Rectangle : "+rectangle.getStroke());
         SelectStrokeColor selectStrokeColorRectangle = new SelectStrokeColor(rectangle, strokeColor);
         selectStrokeColorRectangle.execute();
+        System.out.println("Stroke color of Rectangle after SelectStrokeColor execute: "+rectangle.getStroke());
         assertEquals(strokeColor, rectangle.getStroke());
 
+        System.out.println("\n--------  TEST CASE 2  --------\n");
+
         Ellipse ellipse = new Ellipse();
+        System.out.println("Stroke color of Ellipse : "+ellipse.getStroke());
         SelectStrokeColor selectStrokeColorEllipse = new SelectStrokeColor(ellipse, strokeColor);
         selectStrokeColorEllipse.execute();
+        System.out.println("Stroke color of Ellipse after SelectStrokeColor execute: "+ellipse.getStroke());
         assertEquals(strokeColor, ellipse.getStroke());
 
+        System.out.println("\n--------  TEST CASE 3  --------\n");
+
         Line line = new Line();
+        System.out.println("Stroke color of Line : "+line.getStroke());
         SelectStrokeColor selectStrokeColorLine = new SelectStrokeColor(line, strokeColor);
         selectStrokeColorLine.execute();
+        System.out.println("Stroke color of Line after SelectStrokeColor execute: "+line.getStroke());
         assertEquals(strokeColor, line.getStroke());
 
     }
